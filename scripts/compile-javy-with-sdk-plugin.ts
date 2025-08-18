e34ed09ec4fdd32f26b271a20602c064a6dc4fe8
ernest-nowacki <ernest.nowacki@smartcontract.com>
2025-08-18 16:51:19 +0200

    Fix the generate plugin script so it creates dist directory if its not there yet

--- scripts/compile-javy-with-sdk-plugin.ts
+++ scripts/compile-javy-with-sdk-plugin.ts
@@ -3,6 +3,8 @@ import { $ } from "bun";
 export const main = async () => {
   console.info("\n\n---> Compile Javy SDK plugin \n\n");
 
+  await $`mkdir -p dist`;
+
   await $`bun javy init-plugin plugins/javy_chainlink_sdk/target/wasm32-wasip1/release/javy_chainlink_sdk.wasm -o dist/javy-chainlink-sdk.plugin.wasm`;
 
   console.info("Done!");
