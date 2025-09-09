3020d17befc5ad9d7f0e84383a6e8518cecd96d8
ernest-nowacki <ernest.nowacki@smartcontract.com>
2025-09-09 15:53:34 +0200

    Formatting

--- src/sdk/testhelpers/dangerously-call-capability.ts
+++ src/sdk/testhelpers/dangerously-call-capability.ts
@@ -1,11 +1,8 @@
-import { doRequestAsync } from "@cre/sdk/utils/do-request-async";
-import { awaitAsyncRequest } from "@cre/sdk/utils/await-async-request";
-import {
-  Mode,
-  type CapabilityResponse,
-} from "@cre/generated/sdk/v1alpha/sdk_pb";
-import { LazyPromise } from "@cre/sdk/utils/lazy-promise";
-import { type CallCapabilityParams } from "@cre/sdk/utils/capabilities/call-capability";
+import { doRequestAsync } from '@cre/sdk/utils/do-request-async'
+import { awaitAsyncRequest } from '@cre/sdk/utils/await-async-request'
+import { Mode, type CapabilityResponse } from '@cre/generated/sdk/v1alpha/sdk_pb'
+import { LazyPromise } from '@cre/sdk/utils/lazy-promise'
+import { type CallCapabilityParams } from '@cre/sdk/utils/capabilities/call-capability'
 
 /**
  * Dangerous call capability.
@@ -15,23 +12,23 @@ import { type CallCapabilityParams } from "@cre/sdk/utils/capabilities/call-capa
  * @see callCapability implementation for actual proper usage.
  * */
 export function dangerouslyCallCapability({
-  capabilityId,
-  method,
-  mode = Mode.DON,
-  payload,
+	capabilityId,
+	method,
+	mode = Mode.DON,
+	payload,
 }: CallCapabilityParams): Promise<CapabilityResponse> {
-  const callbackId = doRequestAsync({
-    capabilityId,
-    method,
-    mode,
-    payload,
-  });
+	const callbackId = doRequestAsync({
+		capabilityId,
+		method,
+		mode,
+		payload,
+	})
 
-  return new LazyPromise(async () => {
-    return awaitAsyncRequest(callbackId, {
-      capabilityId,
-      method,
-      mode,
-    });
-  });
+	return new LazyPromise(async () => {
+		return awaitAsyncRequest(callbackId, {
+			capabilityId,
+			method,
+			mode,
+		})
+	})
 }
--- src/workflows/standard_tests/mode_switch/successful_mode_switch/test.ts
+++ src/workflows/standard_tests/mode_switch/successful_mode_switch/test.ts
@@ -1,74 +1,69 @@
-import { AggregationType, Mode } from "@cre/generated/sdk/v1alpha/sdk_pb";
-import { SimpleConsensusInputsSchema } from "@cre/generated/sdk/v1alpha/sdk_pb";
-import { ValueSchema } from "@cre/generated/values/v1/values_pb";
-import { create, toJson } from "@bufbuild/protobuf";
-import { BasicActionCapability } from "@cre/generated-sdk/capabilities/internal/basicaction/v1/basicaction_sdk_gen";
-import { BasicActionCapability as NodeActionCapability } from "@cre/generated-sdk/capabilities/internal/nodeaction/v1/basicaction_sdk_gen";
-import {
-  consensusFieldsFrom,
-  observationValue,
-} from "@cre/sdk/utils/values/consensus";
-import { type NodeRuntime } from "@cre/sdk/runtime/runtime";
-import { BasicCapability as BasicTriggerCapability } from "@cre/generated-sdk/capabilities/internal/basictrigger/v1/basic_sdk_gen";
-import { cre, type Runtime } from "@cre/sdk/cre";
+import { AggregationType, Mode } from '@cre/generated/sdk/v1alpha/sdk_pb'
+import { SimpleConsensusInputsSchema } from '@cre/generated/sdk/v1alpha/sdk_pb'
+import { ValueSchema } from '@cre/generated/values/v1/values_pb'
+import { create, toJson } from '@bufbuild/protobuf'
+import { BasicActionCapability } from '@cre/generated-sdk/capabilities/internal/basicaction/v1/basicaction_sdk_gen'
+import { BasicActionCapability as NodeActionCapability } from '@cre/generated-sdk/capabilities/internal/nodeaction/v1/basicaction_sdk_gen'
+import { consensusFieldsFrom, observationValue } from '@cre/sdk/utils/values/consensus'
+import { type NodeRuntime } from '@cre/sdk/runtime/runtime'
+import { BasicCapability as BasicTriggerCapability } from '@cre/generated-sdk/capabilities/internal/basictrigger/v1/basic_sdk_gen'
+import { cre, type Runtime } from '@cre/sdk/cre'
 
 // Doesn't matter for this test
-type Config = any;
+type Config = any
 
 const handler = async (_config: Config, runtime: Runtime) => {
-  runtime.now();
+	runtime.now()
 
-  const donInput = { inputThing: true };
-  const basicActionCapability = new BasicActionCapability();
-  const donResponse = await basicActionCapability.performAction(donInput);
+	const donInput = { inputThing: true }
+	const basicActionCapability = new BasicActionCapability()
+	const donResponse = await basicActionCapability.performAction(donInput)
 
-  const consensusOutput = await cre.runInNodeMode(
-    async (nodeRuntime: NodeRuntime) => {
-      nodeRuntime.now();
-      const nodeActionCapability = new NodeActionCapability();
-      const nodeResponse = await nodeActionCapability.performAction({
-        inputThing: true,
-      });
+	const consensusOutput = await cre.runInNodeMode(async (nodeRuntime: NodeRuntime) => {
+		nodeRuntime.now()
+		const nodeActionCapability = new NodeActionCapability()
+		const nodeResponse = await nodeActionCapability.performAction({
+			inputThing: true,
+		})
 
-      return create(SimpleConsensusInputsSchema, {
-        observation: observationValue(
-          cre.utils.val.mapValue({
-            OutputThing: cre.utils.val.int64(nodeResponse.outputThing),
-          })
-        ),
-        descriptors: consensusFieldsFrom({
-          OutputThing: AggregationType.MEDIAN,
-        }),
-        default: cre.utils.val.mapValue({
-          OutputThing: cre.utils.val.int64(123),
-        }),
-      });
-    }
-  );
+		return create(SimpleConsensusInputsSchema, {
+			observation: observationValue(
+				cre.utils.val.mapValue({
+					OutputThing: cre.utils.val.int64(nodeResponse.outputThing),
+				}),
+			),
+			descriptors: consensusFieldsFrom({
+				OutputThing: AggregationType.MEDIAN,
+			}),
+			default: cre.utils.val.mapValue({
+				OutputThing: cre.utils.val.int64(123),
+			}),
+		})
+	})
 
-  runtime.now();
-  const outputJson = toJson(ValueSchema, consensusOutput);
+	runtime.now()
+	const outputJson = toJson(ValueSchema, consensusOutput)
 
-  cre.sendResponseValue(
-    cre.utils.val.string(
-      `${donResponse.adaptedThing}${outputJson?.mapValue?.fields?.OutputThing?.int64Value}`
-    )
-  );
-};
-
-const initWorkflow = () => {
-  const basicTrigger = new BasicTriggerCapability();
-
-  return [cre.handler(basicTrigger.trigger({}), handler)];
-};
-
-export async function main() {
-  console.log(
-    `TS workflow: standard test: mode_switch: successful_mode_switch [${new Date().toISOString()}]`
-  );
-
-  const runner = await cre.newRunner();
-  await runner.run(initWorkflow);
+	cre.sendResponseValue(
+		cre.utils.val.string(
+			`${donResponse.adaptedThing}${outputJson?.mapValue?.fields?.OutputThing?.int64Value}`,
+		),
+	)
 }
 
-cre.withErrorBoundary(main);
+const initWorkflow = () => {
+	const basicTrigger = new BasicTriggerCapability()
+
+	return [cre.handler(basicTrigger.trigger({}), handler)]
+}
+
+export async function main() {
+	console.log(
+		`TS workflow: standard test: mode_switch: successful_mode_switch [${new Date().toISOString()}]`,
+	)
+
+	const runner = await cre.newRunner()
+	await runner.run(initWorkflow)
+}
+
+cre.withErrorBoundary(main)
