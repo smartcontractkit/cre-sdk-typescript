e8adfb15e47dee5ca73739f1194bcf9ff935e6b1
Jeff Rifwald <jeff.rifwald@smartcontract.com>
2025-09-11 04:09:06 -0400

    feat(DAPP-7237): pass platform to build script

--- scripts/build-for-cli.ts
+++ scripts/build-for-cli.ts
@@ -8,24 +8,41 @@ const { values } = parseArgs({
   options: {
     input: {
       type: 'string',
+      default: 'main.ts',
     },
     output: {
       type: 'string',
+      default: 'tmp.wasm',
     },
+    platform: {
+      type: 'string',
+      default: 'arm-macos',
+    }
   },
   strict: true,
   allowPositionals: true,
 });
 
+function getJavyBin (platform: string) {
+  switch (platform) {
+    case 'arm-macos':
+      return 'javy-arm-macos-v5.0.4';
+    case 'arm-linux':
+      return 'javy-arm-linux-v5.0.4'
+    default:
+      throw new Error(`platform not supported: ${platform}`);
+  }
+};
+
 async function main() {
   const outputFolder = 'dist';
-  const inputTSFilename = values.input || 'main.ts';
-  const outputWasmFilename = values.output || 'tmp.wasm';
+  const inputTSFilename = values.input;
+  const outputWasmFilename = values.output;
   const inputJSFilename = inputTSFilename.replace(/\.ts$/, '.js');
   const outputJSFilename = outputWasmFilename.replace(/\.wasm$/, '.js');
   const outputJSPath = join(outputFolder, outputJSFilename);
   const packagePath = join('node_modules', 'cre-sdk-typescript');
-  const javy = join(packagePath, 'bin', 'javy-arm-macos-v5.0.4');
+  const javy = join(packagePath, 'bin', getJavyBin(values.platform));
   const sdkPluginPath = join(packagePath, 'dist', 'javy-chainlink-sdk.plugin.wasm');
   const workflowWitPath = join(packagePath, 'src', 'workflows', 'workflow.wit');
 
