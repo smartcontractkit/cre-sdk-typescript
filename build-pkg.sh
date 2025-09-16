88351afb54c862f6fe644fc5853ab1916df2cc4f
Fernando Montero <fernando.montero.perez@gmail.com>
2025-09-16 14:46:54 +0100

    cre: build package (#35)
    * cre: build package
    
    * pin rimraf version
    
    * fix lint error on script
    
    * wip build package
    
    * working a lot nicer now
    
    * delete useless scripts
    
    * revet change to compile-to-wasm
    
    * comments cleanup
    
    * separete repo scripts from cli scripts
    
    * add back deleted file
    
    * update liner rules
    
    * add postinstall script
    
    * add post install script and bash script to generate the necessary tarball
    
    * make package compilable to tarball
    
    * remove build types tsconfig
    
    * remove postinstall script, extend build script to add postinstall script after copying the package json

---
+++ build-pkg.sh
@@ -0,0 +1,18 @@
+bun run build:javy:sdk:wasm
+
+bun tsc --project tsconfig.build.json
+
+sh build-bins.sh
+
+cp bin/* dist/bin/
+cp install.js dist/install.js
+
+chmod +x dist/bin/cre-ts
+
+bun rimraf tsconfig.types.tsbuildinfo 
+
+cp package.json dist/package.json
+
+cd dist
+
+bun pm pkg set scripts.postinstall="node ./install.js"
