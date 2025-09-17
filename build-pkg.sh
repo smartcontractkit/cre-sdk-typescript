f02bbaacf616ee9a62eed88b35467090d3284cfa
Fernando Montero <fernando.montero.perez@gmail.com>
2025-09-17 16:54:43 +0100

    Build platform specifc javy plugins and include binary in cre-build package (#49)
    * add comments
    
    * Download javy when building bins
    
    * Add option to build javy for linux
    
    * build bin for specific target and update github action to share generated artifacts
    
    ---------
    
    Co-authored-by: ernest-nowacki <ernest.nowacki@smartcontract.com>

--- build-pkg.sh
+++ build-pkg.sh
@@ -1,10 +1,5 @@
 bun tsc --project tsconfig.build.json
 
-sh build-bins.sh
-
-# update this script so that it builds the plugin for each platform and places it in pkg/cli/[platform]/...
-bun run build:javy:sdk:wasm
-
 mkdir -p dist/bin
 
 cp bin/* dist/bin/
