7acefabe7b52aa5bbbb85cb8afc0753981b6f06f
Fernando Montero <fernando.montero.perez@gmail.com>
2025-09-17 13:19:12 +0100

    Fix Javy path (#50)

--- scripts/compile-javy-with-sdk-plugin.ts
+++ scripts/compile-javy-with-sdk-plugin.ts
@@ -1,11 +1,13 @@
 import { $ } from 'bun'
 
+const cwd = process.cwd()
+
 export const main = async () => {
 	console.info('\n\n---> Compile Javy SDK plugin \n\n')
 
 	await $`mkdir -p dist`
 
-	await $`chmod +x ../bin/javy-arm-macos-v5.0.4`
+	await $`chmod +x ${cwd}/bin/javy-arm-macos-v5.0.4`
 	// for each platform compile the plugin
 
 	await $`bun javy init-plugin plugins/javy_chainlink_sdk/target/wasm32-wasip1/release/javy_chainlink_sdk.wasm -o dist/javy-chainlink-sdk.plugin.wasm`
