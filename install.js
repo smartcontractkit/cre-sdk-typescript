fae03a846f0ab24705bc09c5fcd3a5feb595fe64
Fernando Montero <fernando.montero.perez@gmail.com>
2025-09-18 13:38:56 +0100

    compile javy plugins

--- install.js
+++ install.js
@@ -5,20 +5,24 @@ import https from 'https'
 
 const __dirname = import.meta.dirname;
 
+const NPM_SCOPE = '@chainlink'
+
 // Lookup table for all platforms and binary distribution packages
 const BINARY_DISTRIBUTION_PACKAGES = {
-    'linux-x64': '@chainlink/cre-ts-linux-x64',
-    'linux-arm64': '@chainlink/cre-ts-linux-arm',
-    'darwin-x64': '@chainlink/cre-ts-darwin-x64',
-    'darwin-arm64': '@chainlink/cre-ts-darwin-arm64',
-    'win32-x64': '@chainlink/cre-ts-windows-x64',
+  'linux-x64': 'cre-ts-linux-x64',
+  'linux-arm64': 'cre-ts-linux-arm',
+  'darwin-x64': 'cre-ts-darwin-x64',
+  'darwin-arm64': 'cre-ts-darwin-arm64',
+  'win32-x64': 'cre-ts-windows-x64',
 }
 
 // Adjust the version you want to install. You can also make this dynamic.
-const BINARY_DISTRIBUTION_VERSION = '1.0.0'
+const BINARY_DISTRIBUTION_VERSION = '0.0.3'
 
 // Windows binaries end with .exe so we need to special case them.
 const binaryName = process.platform === 'win32' ? 'cre-build.exe' : 'cre-build'
+const javyBinary = process.platform === 'win32' ? 'javy.exe' : 'javy'
+const javyPluginWasm = "javy_chainlink_sdk.plugin.wasm"
 
 // Determine package name for this platform
 const platformSpecificPackageName =
@@ -83,16 +87,31 @@ function extractFileFromTarball(tarballBuffer, filepath) {
 
 async function downloadBinaryFromNpm() {
   // Download the tarball of the right binary distribution package
+  console.log(`Downloading binary from npm: ${platformSpecificPackageName}`)
+  const url = `https://registry.npmjs.org/${NPM_SCOPE}/${platformSpecificPackageName}/-/${platformSpecificPackageName}-${BINARY_DISTRIBUTION_VERSION}.tgz`
+  console.log(`Downloading binary from npm: ${url}`)
   const tarballDownloadBuffer = await makeRequest(
-    `https://registry.npmjs.org/${platformSpecificPackageName}/-/${platformSpecificPackageName}-${BINARY_DISTRIBUTION_VERSION}.tgz`
+    url
   )
 
   const tarballBuffer = zlib.unzipSync(tarballDownloadBuffer)
+
+  console.log(`Extracting binary to: ${fallbackBinaryPath}`)
 
   // Extract binary from package and write to disk
   fs.writeFileSync(
     fallbackBinaryPath,
     extractFileFromTarball(tarballBuffer, `package/bin/${binaryName}`),
+    { mode: 0o755 } // Make binary file executable
+  )
+  fs.writeFileSync(
+    fallbackBinaryPath,
+    extractFileFromTarball(tarballBuffer, `package/bin/${javyBinary}`),
+    { mode: 0o755 } // Make binary file executable
+  )
+  fs.writeFileSync(
+    fallbackBinaryPath,
+    extractFileFromTarball(tarballBuffer, `package/bin/${javyPluginWasm}`),
     { mode: 0o755 } // Make binary file executable
   )
 }
