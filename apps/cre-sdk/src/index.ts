bf7682a4049d5deaf2679c109bf90a780eb00417
ernest-nowacki <ernest.nowacki@smartcontract.com>
2025-09-19 17:48:13 +0200

    Update structure

--- apps/cre-sdk/src/index.ts
+++ apps/cre-sdk/src/index.ts
@@ -1,3 +1,4 @@
-export * from './sdk'
-export * from './sdk/runtime'
-export * from './sdk/utils'
+export * from "./sdk";
+export * from "./sdk/runtime";
+export * from "./sdk/utils";
+export { withErrorBoundary } from "./sdk/utils/error-boundary";
