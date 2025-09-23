746801881f243b84ca943ccfe5218e70ad8622e8
Ryan Tinianov <tinianov@live.com>
2025-09-23 02:18:34 -0400

    truncate...?

--- src/sdk/wasm/runtime.ts
+++ src/sdk/wasm/runtime.ts
@@ -49,24 +49,32 @@ class WasmRuntimeHelpers implements RuntimeHelpers {
 	}
 
 	await(request: AwaitCapabilitiesRequest, maxResponseSize: bigint): AwaitCapabilitiesResponse {
-		console.log('await: ', maxResponseSize)
-		console.log('await number:', Number(maxResponseSize))
+		// Convert bigint to integer for WASM host binding
+		// it's an i32, so we shouldn't need to trunc it (all i32 numbers are within the range of float64 without percision loss), but somehow it makes a difference...
+		const responseSize = Math.trunc(Number(maxResponseSize))
+
 		const response = hostBindings.awaitCapabilities(
 			toBinary(AwaitCapabilitiesRequestSchema, request),
-			Number(1024 * 1024 * 5),
+			responseSize,
 		)
 		const responseBytes = Array.isArray(response) ? new Uint8Array(response) : response
 		return fromBinary(AwaitCapabilitiesResponseSchema, responseBytes)
 	}
 
 	getSecrets(request: GetSecretsRequest, maxResponseSize: bigint): boolean {
-		return hostBindings.getSecrets(toBinary(GetSecretsRequestSchema, request), maxResponseSize) >= 0
+		// Convert bigint to integer for WASM host binding
+		// it's an i32, so we shouldn't need to trunc it (all i32 numbers are within the range of float64 without percision loss), but somehow it makes a difference...
+		const responseSize = Math.trunc(Number(maxResponseSize))
+		return hostBindings.getSecrets(toBinary(GetSecretsRequestSchema, request), responseSize) >= 0
 	}
 
 	awaitSecrets(request: AwaitSecretsRequest, maxResponseSize: bigint): AwaitSecretsResponse {
+		// Convert bigint to integer for WASM host binding
+		// it's an i32, so we shouldn't need to trunc it (all i32 numbers are within the range of float64 without percision loss), but somehow it makes a difference...
+		const responseSize = Math.trunc(Number(maxResponseSize))
 		const response = hostBindings.awaitSecrets(
 			toBinary(AwaitSecretsRequestSchema, request),
-			maxResponseSize,
+			responseSize,
 		)
 		const responseBytes = Array.isArray(response) ? new Uint8Array(response) : response
 		return fromBinary(AwaitSecretsResponseSchema, responseBytes)
