84c5013b0f161d6438946d183ced5fd813dec40c
ernest-nowacki <ernest.nowacki@smartcontract.com>
2025-09-25 11:42:08 +0200

    Make sure cre is exported

--- packages/cre-sdk/src/sdk/index.ts
+++ packages/cre-sdk/src/sdk/index.ts
@@ -1,3 +1,4 @@
-export type * from './runtime'
-export * from './runtime'
-export * from './workflow'
+export type * from "./runtime";
+export * from "./runtime";
+export * from "./workflow";
+export * from "./cre";
