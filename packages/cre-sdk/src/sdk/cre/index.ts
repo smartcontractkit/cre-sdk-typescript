4e571332cdf75ba526b8869aa2c106a4386e06bf
ernest-nowacki <ernest.nowacki@smartcontract.com>
2025-09-25 15:31:47 +0200

    Run linter

--- packages/cre-sdk/src/sdk/cre/index.ts
+++ packages/cre-sdk/src/sdk/cre/index.ts
@@ -2,26 +2,26 @@
  * Public API for the CRE SDK.
  */
 
-import { ClientCapability as EVMClient } from "@cre/generated-sdk/capabilities/blockchain/evm/v1alpha/client_sdk_gen";
-import { ClientCapability as HTTPClient } from "@cre/generated-sdk/capabilities/networking/http/v1alpha/client_sdk_gen";
-import { HTTPCapability } from "@cre/generated-sdk/capabilities/networking/http/v1alpha/http_sdk_gen";
-import { CronCapability } from "@cre/generated-sdk/capabilities/scheduler/cron/v1/cron_sdk_gen";
-import { prepareRuntime } from "@cre/sdk/utils/prepare-runtime";
-import { handler } from "@cre/sdk/workflow";
+import { ClientCapability as EVMClient } from '@cre/generated-sdk/capabilities/blockchain/evm/v1alpha/client_sdk_gen'
+import { ClientCapability as HTTPClient } from '@cre/generated-sdk/capabilities/networking/http/v1alpha/client_sdk_gen'
+import { HTTPCapability } from '@cre/generated-sdk/capabilities/networking/http/v1alpha/http_sdk_gen'
+import { CronCapability } from '@cre/generated-sdk/capabilities/scheduler/cron/v1/cron_sdk_gen'
+import { prepareRuntime } from '@cre/sdk/utils/prepare-runtime'
+import { handler } from '@cre/sdk/workflow'
 
-export type { Log as EVMLog } from "@cre/generated/capabilities/blockchain/evm/v1alpha/client_pb";
-export type { Payload as HTTPPayload } from "@cre/generated/capabilities/networking/http/v1alpha/trigger_pb";
-export type { Payload as CronPayload } from "@cre/generated/capabilities/scheduler/cron/v1/trigger_pb";
-export type { NodeRuntime, Runtime } from "@cre/sdk/runtime";
+export type { Log as EVMLog } from '@cre/generated/capabilities/blockchain/evm/v1alpha/client_pb'
+export type { Payload as HTTPPayload } from '@cre/generated/capabilities/networking/http/v1alpha/trigger_pb'
+export type { Payload as CronPayload } from '@cre/generated/capabilities/scheduler/cron/v1/trigger_pb'
+export type { NodeRuntime, Runtime } from '@cre/sdk/runtime'
 
-prepareRuntime();
+prepareRuntime()
 
 export const cre = {
-  capabilities: {
-    CronCapability,
-    HTTPCapability,
-    HTTPClient,
-    EVMClient,
-  },
-  handler,
-};
+	capabilities: {
+		CronCapability,
+		HTTPCapability,
+		HTTPClient,
+		EVMClient,
+	},
+	handler,
+}
