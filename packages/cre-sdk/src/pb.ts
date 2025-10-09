9d138134b67e4b630c237dcfd6415dc116b6c85e
Ernest Nowacki <124677192+ernest-nowacki@users.noreply.github.com>
2025-10-09 11:20:58 +0200

    Allow ungroupped imports (#120)

---
+++ packages/cre-sdk/src/pb.ts
@@ -0,0 +1,6 @@
+export * as EVM_PB from '@cre/generated/capabilities/blockchain/evm/v1alpha/client_pb'
+export * as HTTP_CLIENT_PB from '@cre/generated/capabilities/networking/http/v1alpha/client_pb'
+export * as HTTP_TRIGGER_PB from '@cre/generated/capabilities/networking/http/v1alpha/trigger_pb'
+export * as CRON_TRIGGER_PB from '@cre/generated/capabilities/scheduler/cron/v1/trigger_pb'
+export * as SDK_PB from '@cre/generated/sdk/v1alpha/sdk_pb'
+export * as VALUES_PB from '@cre/generated/values/v1/values_pb'
