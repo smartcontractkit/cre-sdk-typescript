66e0896c19ea77ab2518851f705df6dca6d66680
ernest-nowacki <ernest.nowacki@smartcontract.com>
2025-10-28 12:41:10 +0100

    Fix TS issue inside cre-http-trigger

--- packages/cre-http-trigger/src/utils.ts
+++ packages/cre-http-trigger/src/utils.ts
@@ -3,7 +3,7 @@ import stringify from 'json-stable-stringify'
 
 // Helper function to compute SHA256 hash
 export const sha256 = (data: any): string => {
-	const jsonString = typeof data === 'string' ? data : stringify(data)
+	const jsonString = typeof data === 'string' ? data : (stringify(data) ?? '')
 	return createHash('sha256').update(jsonString).digest('hex')
 }
 
