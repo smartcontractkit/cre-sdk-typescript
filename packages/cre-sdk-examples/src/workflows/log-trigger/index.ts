fb696d58deb40aa82ed778214b0fac88e21a8aa5
ernest-nowacki <ernest.nowacki@smartcontract.com>
2026-01-26 07:58:56 +0100

    Add basic log-trigger example

---
+++ packages/cre-sdk-examples/src/workflows/log-trigger/index.ts
@@ -0,0 +1,76 @@
+import {
+  EVMClient,
+  type EVMLog,
+  getNetwork,
+  handler,
+  Runner,
+  type Runtime,
+  safeJsonStringify,
+} from "@chainlink/cre-sdk";
+import { z } from "zod";
+
+const configSchema = z.object({
+  evms: z.array(
+    z.object({
+      messageEmitterAddress: z.string(),
+      chainSelectorName: z.string(),
+    }),
+  ),
+});
+
+type Config = z.infer<typeof configSchema>;
+
+const onLogTrigger = (runtime: Runtime<Config>, payload: EVMLog): string => {
+  runtime.log("Running LogTrigger");
+
+  const topics = payload.topics;
+
+  if (topics.length < 3) {
+    runtime.log("Log payload does not contain enough topics");
+    throw new Error(
+      `log payload does not contain enough topics ${topics.length}`,
+    );
+  }
+
+  runtime.log(`Log payload: ${safeJsonStringify(payload)}`);
+
+  if (!payload.blockNumber) {
+    throw new Error("Block number is required");
+  }
+
+  runtime.log(`Block number: ${safeJsonStringify(payload.blockNumber)}`);
+
+  return "success";
+};
+
+const initWorkflow = (config: Config) => {
+  const network = getNetwork({
+    chainFamily: "evm",
+    chainSelectorName: config.evms[0].chainSelectorName,
+    isTestnet: true,
+  });
+
+  if (!network) {
+    throw new Error(
+      `Network not found for chain selector name: ${config.evms[0].chainSelectorName}`,
+    );
+  }
+
+  const evmClient = new EVMClient(network.chainSelector.selector);
+
+  return [
+    handler(
+      evmClient.logTrigger({
+        addresses: [config.evms[0].messageEmitterAddress],
+      }),
+      onLogTrigger,
+    ),
+  ];
+};
+
+export async function main() {
+  const runner = await Runner.newRunner<Config>({
+    configSchema,
+  });
+  await runner.run(initWorkflow);
+}
