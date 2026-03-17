edd6401f2b2a4d9960ed4364775d87018ae74692
amit-momin <108959691+amit-momin@users.noreply.github.com>
2026-03-17 18:53:31 -0500

    Update chain-selectors dependency (#210)
    * Updated chain-selectors dependency
    
    * Updated package.json version

---
+++ packages/cre-sdk/src/generated/chain-selectors/testnet/evm/tempo-testnet.ts
@@ -0,0 +1,16 @@
+// This file is auto-generated. Do not edit manually.
+// Generated from: https://github.com/smartcontractkit/chain-selectors
+
+import type { NetworkInfo } from '@cre/sdk/utils/chain-selectors/types'
+
+const network: NetworkInfo = {
+	chainId: '42429',
+	chainSelector: {
+		name: 'tempo-testnet',
+		selector: 3963528237232804922n,
+	},
+	chainFamily: 'evm',
+	networkType: 'testnet',
+} as const
+
+export default network
