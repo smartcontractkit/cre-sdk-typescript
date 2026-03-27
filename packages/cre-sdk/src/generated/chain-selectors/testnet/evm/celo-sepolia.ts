c2adc5963ecf18f29377d0490739901e7246734b
ernest-nowacki <ernest.nowacki@smartcontract.com>
2026-03-27 11:17:35 +0100

    Run bun-full checks with new docker support

---
+++ packages/cre-sdk/src/generated/chain-selectors/testnet/evm/celo-sepolia.ts
@@ -0,0 +1,16 @@
+// This file is auto-generated. Do not edit manually.
+// Generated from: https://github.com/smartcontractkit/chain-selectors
+
+import type { NetworkInfo } from '@cre/sdk/utils/chain-selectors/types'
+
+const network: NetworkInfo = {
+	chainId: '11142220',
+	chainSelector: {
+		name: 'celo-sepolia',
+		selector: 3761762704474186180n,
+	},
+	chainFamily: 'evm',
+	networkType: 'testnet',
+} as const
+
+export default network
