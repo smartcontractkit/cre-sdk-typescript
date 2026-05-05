4f85f23eea6f353bcb4b6e86ee2acfcea60f08cb
Russell Stern <russell.stern@smartcontract.com>
2026-05-05 15:40:40 -0400

    Added getSecrets method to fetch multiple secrets

--- packages/cre-sdk/src/sdk/workflow.ts
+++ packages/cre-sdk/src/sdk/workflow.ts
@@ -1,19 +1,18 @@
-import type { Message } from '@bufbuild/protobuf'
+import type { Message } from "@bufbuild/protobuf";
 import type {
-	CapabilityResponse,
 	Secret,
 	SecretRequest,
 	SecretRequestJson,
-} from '@cre/generated/sdk/v1alpha/sdk_pb'
-import { type Runtime } from '@cre/sdk/runtime'
-import type { Trigger } from '@cre/sdk/utils/triggers/trigger-interface'
-import type { SecretsError } from './errors'
-import type { CreSerializable } from './utils'
+	SecretResponse,
+} from "@cre/generated/sdk/v1alpha/sdk_pb";
+import type { Runtime } from "@cre/sdk/runtime";
+import type { Trigger } from "@cre/sdk/utils/triggers/trigger-interface";
+import type { CreSerializable } from "./utils";
 
 export type HandlerFn<TConfig, TTriggerOutput, TResult> = (
 	runtime: Runtime<TConfig>,
 	triggerOutput: TTriggerOutput,
-) => Promise<CreSerializable<TResult>> | CreSerializable<TResult>
+) => Promise<CreSerializable<TResult>> | CreSerializable<TResult>;
 
 export interface HandlerEntry<
 	TConfig,
@@ -21,11 +20,13 @@ export interface HandlerEntry<
 	TTriggerOutput,
 	TResult,
 > {
-	trigger: Trigger<TRawTriggerOutput, TTriggerOutput>
-	fn: HandlerFn<TConfig, TTriggerOutput, TResult>
+	trigger: Trigger<TRawTriggerOutput, TTriggerOutput>;
+	fn: HandlerFn<TConfig, TTriggerOutput, TResult>;
 }
 
-export type Workflow<TConfig> = ReadonlyArray<HandlerEntry<TConfig, any, any, any>>
+export type Workflow<TConfig> = ReadonlyArray<
+	HandlerEntry<TConfig, any, any, any>
+>;
 
 export const handler = <
 	TRawTriggerOutput extends Message<string>,
@@ -38,10 +39,13 @@ export const handler = <
 ): HandlerEntry<TConfig, TRawTriggerOutput, TTriggerOutput, TResult> => ({
 	trigger,
 	fn,
-})
+});
 
 export type SecretsProvider = {
+	getSecrets(requests: Array<SecretRequest | SecretRequestJson>): {
+		result: () => SecretResponse[];
+	};
 	getSecret(request: SecretRequest | SecretRequestJson): {
-		result: () => Secret
-	}
-}
+		result: () => Secret;
+	};
+};
