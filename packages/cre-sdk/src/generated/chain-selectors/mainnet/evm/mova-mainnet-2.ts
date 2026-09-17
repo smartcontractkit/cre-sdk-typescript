534854d0bf4432e4d8776dfd4db0c749319ce679
Krish-vemula <krishnateja.vemula@smartcontract.com>
2026-09-16 18:02:58 -0700

    cre/bump sdk for add ethereum hoodi - capdev

---
+++ packages/cre-sdk/src/generated/chain-selectors/mainnet/evm/mova-mainnet-2.ts
@@ -0,0 +1,16 @@
+// This file is auto-generated. Do not edit manually.
+// Generated from: https://github.com/smartcontractkit/chain-selectors
+
+import type { NetworkInfo } from '@cre/sdk/utils/chain-selectors/types'
+
+const network: NetworkInfo = {
+	chainId: '61901',
+	chainSelector: {
+		name: 'mova-mainnet-2',
+		selector: 4215185756725900654n,
+	},
+	chainFamily: 'evm',
+	networkType: 'mainnet',
+} as const
+
+export default network
